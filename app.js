d07bd63ff07c8582e618b3b4fc5c68a7cbf7627f
Fabio-Alfredo <00210121@uca.edu.sv>
2024-11-21 17:37:59 -0600

    add: cors

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import { config } from './src/configs/config.js';
 import { dbConnection } from './src/configs/dbConnection.config.js';
 import  routes  from './src/routes/index.route.js';
 import { errorHandler } from './src/middlewares/errorHanddler.middleware.js';
+import cors from 'cors';
 
 const app =express();
 
 const PORT = config.port|| 3002;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api", routes);
